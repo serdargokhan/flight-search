d0f3632f688ca84ef8a2f02a4e987494b9f3570c
serdargokhan <gokhan_bakirci_53@hotmail.com>
2023-09-02 17:29:11 +0300

    feat: add card shadow and abstract primary color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
+import colors from "tailwindcss/colors";
 
 const config = {
     content: ["./src/**/*.{ts,tsx,mdx}"],
@@ -9,9 +10,17 @@ const config = {
             padding: "1rem",
             screens: { xl: "1280px" }
         },
+        extend: {
+            colors: {
+                primary: { ...colors.blue }
+            },
             fontFamily: {
                 sans: ["var(--inter-font)", ...fontFamily.sans]
             },
+            boxShadow: {
+                card: "0px 4px 12px rgba(0, 0, 0, 0.1)"
+            }
+        }
     },
     future: {
         hoverOnlyWhenSupported: true
